08573ab348710bf239465ced41155fa53d791c71
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-07-24 11:55:39 -0700

    implemented vmss manual scaling, just comment out web-linuxvm files

---
+++ c7-web-linux-vmss-input-variables.tf
@@ -0,0 +1,5 @@
+variable "web_vmss_nsg_inbound_ports" {
+    description = "Web VMSS NSG inbound ports"
+    type = list(string) # can also use set or map, diff implementations tho
+    default = [22,80,443] # key = 0,1,2
+}
