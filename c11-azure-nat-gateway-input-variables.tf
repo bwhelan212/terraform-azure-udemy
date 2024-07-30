329b6cd9c95d795aa44f36557be91b79b1fa5c97
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-07-29 23:01:19 -0700

    nat gateway for internal lb/app tier

---
+++ c11-azure-nat-gateway-input-variables.tf
@@ -0,0 +1,1 @@
+# input variables for nat gateway
---
+++ c11-azure-nat-gateway-outputs.tf
@@ -0,0 +1,11 @@
+# NAT gateway ID 
+output "nat_gw_id" {
+    description = "Azure NAT Gateway ID"
+    value = azurerm_nat_gateway.natgw.id
+}
+
+# NAT gateway public IP ID
+output "nat_gw_public_ip" {
+    description = "Azure NAT Gateway Public IP addy"
+    value = azurerm_public_ip.natgw_publicip.ip_address
+}
