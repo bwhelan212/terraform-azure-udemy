fbdec721a6a580bdfc231dd908f7de0d858bb374
Brody Whelan <bwhelan212@csu.fullerton.edu>
2024-08-01 15:16:15 -0700

    maded changes for outdated info

--- 25-Azure-Traffic-Manager/project-3-azure-traffic-manager/c6-traffic-manager.tf
+++ 25-Azure-Traffic-Manager/project-3-azure-traffic-manager/c6-traffic-manager.tf
@@ -10,7 +10,7 @@ resource "azurerm_traffic_manager_profile" "tm_profile" {
   }
 
   monitor_config {
-    protocol                     = "http"
+    protocol                     = "HTTP"
     port                         = 80
     path                         = "/"
     interval_in_seconds          = 30
@@ -22,22 +22,18 @@ resource "azurerm_traffic_manager_profile" "tm_profile" {
 }
 
 # Traffic Manager Endpoint - Project-1-EastUs2
-resource "azurerm_traffic_manager_endpoint" "tm_endpoint_project1_eastus2" {
+resource "azurerm_traffic_manager_azure_endpoint" "tm_endpoint_project1_eastus2" {
   name                = "tm-endpoint-project1-eastus2"
-  resource_group_name = azurerm_resource_group.rg.name
-  profile_name        = azurerm_traffic_manager_profile.tm_profile.name
-  type                = "azureEndpoints"
+  profile_id          = azurerm_traffic_manager_profile.tm_profile.id
   target_resource_id  = data.terraform_remote_state.project1_eastus2.outputs.web_lb_public_ip_address_id
   weight              = 50
 }
 
 
 # Traffic Manager Endpoint - Project-2-WestUs2
-resource "azurerm_traffic_manager_endpoint" "tm_endpoint_project2_westus2" {
+resource "azurerm_traffic_manager_azure_endpoint" "tm_endpoint_project2_westus2" {
   name                = "tm-endpoint-project2-westus2"
-  resource_group_name = azurerm_resource_group.rg.name
-  profile_name        = azurerm_traffic_manager_profile.tm_profile.name
-  type                = "azureEndpoints"
+  profile_id          = azurerm_traffic_manager_profile.tm_profile.id  
   target_resource_id  = data.terraform_remote_state.project2_westus2.outputs.web_lb_public_ip_address_id 
   weight              = 50
 }
